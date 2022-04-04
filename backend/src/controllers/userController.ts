9bd824c604a78cbc46b1b06f7af8fd77059795e5
Isaac Willian <isaaacwillian@gmail.com>
2022-04-04 16:56:07 -0300

    Add: bcrypt lib for password

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,14 +1,13 @@
 import User from "../models/User";
+import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
-
-interface props {}
 
 const userController = {
     register: (req: Request, res: Response) => {
         const user = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: bcrypt.hashSync(req.body.password),
         });
 
         user.save()
@@ -18,6 +17,17 @@ const userController = {
             .catch((err: Error) => {
                 res.status(400).send(err.message);
             });
+    },
+
+    login: async (req: Request, res: Response) => {
+        const selectedUser = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        });
+        if (!selectedUser)
+            return res.status(400).send("Email or password is incorrect");
+
+        res.send("User Logged");
     },
 };
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,5 @@ mongoose.connect("mongodb://127.0.0.1/users", (err) => {
 });
 
 app.use("/user", express.json(), userRouter);
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
 
 app.listen(3333, () => console.log("Server is running"));
