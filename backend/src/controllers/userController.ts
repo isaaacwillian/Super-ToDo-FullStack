742e110b62df4c479414a963551d72df208aedff
Isaac Willian <isaaacwillian@gmail.com>
2022-04-05 11:58:49 -0300

    [BACKEND] Add: Joi for validation

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,11 +1,15 @@
 import User from "../models/User";
 import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
+import { loginValidate, registerValidate } from "./validate";
 
 const userController = {
   register: (req: Request, res: Response) => {
+    const { error } = registerValidate(req.body);
+    if (error) return res.status(400).send(error.message);
+
     const user = new User({
-      name: req.body.name,
+      username: req.body.username,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password),
     });
@@ -16,11 +20,14 @@ const userController = {
         res.send(user);
       })
       .catch((err: Error) => {
-        res.status(400).send(err.message);
+        res.status(400).send(err);
       });
   },
 
   login: async (req: Request, res: Response) => {
+    const { error } = loginValidate(req.body);
+    if (error) return res.status(400).send(error.message);
+
     const selectedUser = await User.findOne({
       email: req.body.email,
     });
